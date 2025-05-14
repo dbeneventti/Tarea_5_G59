e38da041e3898b5023a01f44214f4b523a49ba63
dbeneventti <82625905+dbeneventti@users.noreply.github.com>
2025-05-13 21:31:59 -0400

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
 import streamlit as st
+import pandas as pd
 
-st.title("¡Hola, Streamlit! 👋")
-st.write("Esta es mi primera app desplegada en Streamlit Cloud.")
+df = pd.read_csv("https://github.com/dbeneventti/Tarea_5_G59/blob/main/data.csv", parse_dates=["Date"])
 
-x = st.slider("Selecciona un valor", 0, 100, 50)
-st.write("Has seleccionado:", x)
+st.title("Dashboard Ventas - Grupo 59")
+
+st.subheader("Filtros")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    ciudad = st.selectbox("Ciudad", df["City"].unique())
+
+with col2:
+    genero = st.multiselect("Género", df["Gender"].unique(), default=df["Gender"].unique())
+
+with col3:
+    metodo_pago = st.multiselect("Método de Pago", df["Payment"].unique(), default=df["Payment"].unique())
+
+
+df_filtrado = df[
+    (df["City"] == ciudad) &
+    (df["Gender"].isin(genero)) &
+    (df["Payment"].isin(metodo_pago))
+]
+
+
+st.dataframe(df_filtrado)
