dda8dba78bb15d6e53a4e1cb62d3534a275ef0eb
dbeneventti <82625905+dbeneventti@users.noreply.github.com>
2025-05-13 21:40:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,28 +8,23 @@ df = pd.read_csv("https://raw.githubusercontent.com/dbeneventti/Tarea_5_G59/main
 st.title("📊 Dashboard Ventas - Grupo 59")
 
 # Filtros dentro de un expander
-with st.expander("🔍 Filtros (haz clic para mostrar u ocultar)", expanded=True):
+with st.expander("🔍 Filtros", expanded=False):
     col1, col2, col3, col4, col5 = st.columns(5)
 
     with col1:
-        ciudades = df["City"].unique()
-        ciudades_sel = st.multiselect("Ciudad", ciudades, default=ciudades)
+        ciudad = st.multiselect("Ciudad", df["City"].unique())
 
     with col2:
-        generos = df["Gender"].unique()
-        generos_sel = st.multiselect("Género", generos, default=generos)
+        genero = st.multiselect("Género", df["Gender"].unique())
 
     with col3:
-        pagos = df["Payment"].unique()
-        pagos_sel = st.multiselect("Método de Pago", pagos, default=pagos)
+        pago = st.multiselect("Pago", df["Payment"].unique())
 
     with col4:
-        productos = df["Product line"].unique()
-        productos_sel = st.multiselect("Línea de producto", productos, default=productos)
+        producto = st.multiselect("Producto", df["Product line"].unique())
 
     with col5:
-        clientes = df["Customer type"].unique()
-        clientes_sel = st.multiselect("Tipo de Cliente", clientes, default=clientes)
+        cliente = st.multiselect("Cliente", df["Customer type"].unique())
 
 # Aplicar filtros
 df_filtrado = df[
